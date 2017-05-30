ce42e637a865385550b923c4d424619ffc0a5a6b
LarryMckuydee <larry.mckuydee@gmail.com>
2017-05-30 16:47:12 +0800

    remove request from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # byebug
-  auth = request.env['omniauth.auth']
-  render json: auth.to_json
+  # auth = request.env['omniauth.auth']
+  # render json: auth.to_json
 end
