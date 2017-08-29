2d34b3d3dfd88dc0fcef10fa43d7a26a957a46b0
LarryMckuydee <larry.mckuydee@gmail.com>
2017-08-29 11:24:16 +0800

    Histories page to show changes made - link doesn't show in sidebar - hidden from normal use, use when shit happened so that admin can keep track

--- config/routes.rb
+++ config/routes.rb
@@ -48,6 +48,7 @@ Rails.application.routes.draw do
   resources :welcome
   resources :notifications
   resources :samples
+  resources :histories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
