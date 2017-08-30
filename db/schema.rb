8a8ae0279f819cb5af7b8eb49111f35d9baf3655
Sean Yeoh <sean.yeoh@hotmail.com>
2017-08-30 12:00:34 +0800

    Add name to users. Accept name from oauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828064107) do
+ActiveRecord::Schema.define(version: 20170830040004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -308,6 +308,7 @@ ActiveRecord::Schema.define(version: 20170828064107) do
     t.string "uid"
     t.datetime "deleted_at"
     t.integer "roles_mask"
+    t.string "name"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
