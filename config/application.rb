82878493e8a77b9f9c76f13fa15258191f06a51d
LarryMckuydee <larry.mckuydee@gmail.com>
2017-09-06 14:23:33 +0800

    Whitelist webconsole

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Odin
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.web_console.development_only = false
+    config.web_console.whitelisted_ips = '139.162.121.87'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
