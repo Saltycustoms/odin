7ddf3d21c88ea49b51ae44089ad117decc1d6e68
Larry Mckuydee <larry.mckuydee@gmail.com>
2017-10-24 14:58:38 +0800

    Show project name on samples

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -2,4 +2,9 @@ class Sample < ActiveResourceRecord
   include NotificationHandler
   self.site = "#{Figaro.env.locate_procurement_app}/api/v1"
   belongs_to :project
+
+  def deal_name
+    project = self.project
+    "#{project.deal_id}##{project.deal_name}"
+  end
 end
