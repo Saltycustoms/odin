c1c271a0570997ab603fb3ac51b95ff970ea376c
Larry Mckuydee <larry.mckuydee@gmail.com>
2017-10-27 11:21:17 +0800

    Feedback function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,6 +26,7 @@
 //= require scripts/toolkit
 //= require tagmanager
 //= require jssha256.js
+//= require feedback.js
 
 $(document).on('turbolinks:load', function() {
     jQuery(".best_in_place").best_in_place();
